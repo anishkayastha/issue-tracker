755284753a57d0f70235cb7c83ae961fbb22b029
anish kayastha <anish@GEONET.COM>
2023-12-25 16:42:13 +0545

    first commit

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react'
+import AddtoCart from './AddtoCart';
+// import styles from './ProductCard.module.css';
+
+const ProductCard = () => {
+  return (
+    <div>
+        <AddtoCart />
+    </div>
+  )
+}
+
+export default ProductCard
