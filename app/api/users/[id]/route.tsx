09e346ca9bc88d13e812da1e8a6737f843b31840
anish kayastha <anish.kayastha08@gmail.com>
2023-12-27 13:19:45 +0545

    starting prisma

---
+++ app/api/users/[id]/route.tsx
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
+
+export function GET(
+    request: NextRequest, 
+    { params }: { params: { id: number } }) {
+    // Fetch data from a db
+    // If not found, return 404 error
+    // Else return data
+        if (params.id > 10)
+            return NextResponse.json({ error: 'User not found'}, { status: 404 });
+
+        return NextResponse.json({ id: 1, name: 'Mosh'});
+}
+
+export async function PUT(
+    request: NextRequest,
+    { params }: { params: { id : number } }) {
+    const body = await request.json();
+    const validation = schema.safeParse(body);
+
+    // Validate the request body
+    // If invalid, return 400
+    // Fetch the user with the given id
+    // If doesn't exist, return 404
+    // Update the user
+    // Return the updated user
+    if (!validation.success)
+        return NextResponse.json(validation.error.errors, { status: 400 });
+
+    if (params.id > 10)
+        return NextResponse.json({ error: 'User not found'}, { status: 404 });
+
+    return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+    request: NextRequest,
+    { params }: { params: { id: number } }) {
+    // Fetch user form db
+    // If not found, return 404
+    // Delete the user
+    // Return 200
+    if (params.id > 10)
+        return NextResponse.json({ error: 'User not found'}, { status: 404 });
+
+    return NextResponse.json({});
+}
